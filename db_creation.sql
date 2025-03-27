89d67781bf0c98ae4564567af0c71422be89551e
evdevarap <155490320+evdevarap@users.noreply.github.com>
2025-03-27 16:13:47 -0400

    added login table create statement

--- db_creation.sql
+++ db_creation.sql
@@ -1,1 +1,12 @@
 -- sample commit by Ethan (comment)
+
+
+-- create login table
+
+CREATE TABLE IF NOT EXISTS Login (
+	loginID SERIAL PRIMARY KEY,
+	Username TEXT NOT NULL UNIQUE,
+	Password TEXT NOT NULL
+);
+
+
