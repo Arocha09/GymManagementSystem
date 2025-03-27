2201cabf6700a53a23d18b41218a3d2205ccd27f
evdevarap <155490320+evdevarap@users.noreply.github.com>
2025-03-27 16:19:06 -0400

    added address table create statement

--- db_creation.sql
+++ db_creation.sql
@@ -10,3 +10,13 @@ CREATE TABLE IF NOT EXISTS Login (
 );
 
 
+-- create address table
+
+CREATE TABLE IF NOT EXISTS Address (
+	addressID SERIAL PRIMARY KEY,
+	StName TEXT NOT NULL,
+	City TEXT NOT NULL,
+	State TEXT NOT NULL,
+	Zip CHAR(5) NOT NULL CHECK (Zip ~ '^[0-9]{5}$') -- note that I made zip code a chr variable with exactly 5 digits only, which is why the regex is in there
+);
+
