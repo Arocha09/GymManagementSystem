3f75d113a815d22ede20adb09606403838734131
Aiden Rocha <126905791+Arocha09@users.noreply.github.com>
2025-03-28 13:44:17 -0400

    Facilities and Gym create Tables

--- db_creation.sql
+++ db_creation.sql
@@ -20,3 +20,27 @@ CREATE TABLE IF NOT EXISTS Address (
 	Zip CHAR(5) NOT NULL CHECK (Zip ~ '^[0-9]{5}$') -- note that I made zip code a chr variable with exactly 5 digits only, which is why the regex is in there
 );
 
+
+-- Create Gym table
+-- Note time columns for open and close not a single column
+
+CREATE TABLE IF NOT EXISTS Gym (
+    gymID SERIAL PRIMARY KEY,
+    gymName TEXT NOT NULL,
+    gymOpen TIME NOT NULL,
+    gymClose TIME NOT NULL,
+    adminID INT REFERENCES Person(personID),
+    addressID INT REFERENCES Address(addressID)
+);
+
+
+-- Create Facilities Table
+-- Note time columns for open and close not a single column
+
+CREATE TABLE IF NOT EXISTS Facilities(
+    facilityID SERIAL PRIMARY KEY,
+    facilityName TEXT NOT NULL,
+    facilityOpen TIME NOT NULL,
+    facilityClose TIME NOT NULL,
+    gymID INT REFERENCES Gym(gymID)
+);
